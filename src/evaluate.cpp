b325ff8a48a99873a7e7b41f6df0987bab0d9dd7
Gary Linscott <glinscott@gmail.com>
2013-04-24 08:21:18 -0400

    Reduce score

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -957,7 +957,7 @@ Value do_evaluate(const Position& pos, Value& margin) {
     } while (b);
 
     if (min_file != max_file) {
-        score += make_score((max_file-min_file-3) * 5, (max_file-min_file-3) * 20);
+        score += make_score(0, (max_file-min_file-3) * 15);
     }
 
     // Add the scores to the middle game and endgame eval
