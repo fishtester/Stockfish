b572f221799cb6f708a50835b5c1cd67a038d888
Gary Linscott <glinscott@gmail.com>
2013-05-26 00:47:55 -0400

    Reduce with depth instead of increase

--- src/search.cpp
+++ src/search.cpp
@@ -727,8 +727,8 @@ namespace {
         &&  excludedMove == MOVE_NONE
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY)
     {
-        Value rbeta = beta + 200;
-        Depth rdepth = depth - (3 * ONE_PLY + depth / 6);
+        Value rbeta = beta + 200 - depth * 4;
+        Depth rdepth = depth - 4 * ONE_PLY;
 
         assert(rdepth >= ONE_PLY);
         assert((ss-1)->currentMove != MOVE_NONE);
