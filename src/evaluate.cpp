7f5ca49f9aa94243098630acb2a559a6ebf90718
Gary Linscott <glinscott@gmail.com>
2013-06-23 22:20:20 -0400

    Less aggressive pinned piece

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -691,9 +691,8 @@ Value do_evaluate(const Position& pos, Value& margin) {
 
     // King safety. This is quite complicated, and is almost certainly far
     // from optimally tuned.
-    if (ei.checkThreat[Them] ||
-        (ei.kingAttackersCount[Them] >= 2 
-         && ei.kingAdjacentZoneAttacksCount[Them]))
+    if ((ei.kingAttackersCount[Them] >= 2 || ei.checkThreat[Them])
+         && ei.kingAdjacentZoneAttacksCount[Them])
     {
         // Find the attacked squares around the king which has no defenders
         // apart from the king itself
