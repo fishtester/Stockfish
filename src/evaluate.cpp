e679aedf998c841d09d5b4a20fac250d7b4ee631
Gary Linscott <glinscott@gmail.com>
2013-06-26 21:52:20 -0400

    Increase value of king adjacent attacks

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -694,7 +694,7 @@ Value do_evaluate(const Position& pos, Value& margin) {
         // attacked and undefended squares around our king, the square of the
         // king, and the quality of the pawn shelter.
         attackUnits =  std::min(25, (ei.kingAttackersCount[Them] * ei.kingAttackersWeight[Them]) / 2)
-                     + 2 * (ei.kingAdjacentZoneAttacksCount[Them] + popcount<Max15>(undefended))
+                     + 4 * (ei.kingAdjacentZoneAttacksCount[Them] + popcount<Max15>(undefended))
                      + KingExposed[relative_square(Us, ksq)]
                      - mg_value(score) / 32;
 
