e0ff3f5c5c79f0692e6c43b412b78a1955059f46
Gary Linscott <glinscott@gmail.com>
2013-07-29 15:24:32 -0400

    Reduce countermoves less in cut nodes

--- src/search.cpp
+++ src/search.cpp
@@ -939,11 +939,10 @@ moves_loop: // When in check and at SpNode search starts from here
       {
           ss->reduction = reduction<PvNode>(depth, moveCount);
 
-          if (!PvNode && cutNode)
-              ss->reduction += ONE_PLY;
-
           if (move == countermoves[0] || move == countermoves[1])
               ss->reduction = std::max(DEPTH_ZERO, ss->reduction-ONE_PLY);
+          else if (!PvNode && cutNode)
+              ss->reduction += ONE_PLY;
 
           Depth d = std::max(newDepth - ss->reduction, ONE_PLY);
           if (SpNode)
